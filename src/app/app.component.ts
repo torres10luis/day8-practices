7fb96ae33fce26b76cf03b0f18e300e57a2962a9
torres10luis <Luis.angel_15@yahoo.com>
2018-06-12 18:21:40 -0700

    practice 1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  show = false;
+  arrs: any[] = [];
+  time: '';
+  state: '';
+
+  // getDetails() {
+  //   this.show = false;
+  //   this.pushArr();
+
+  //   // this.show =!
+  //   // console.log(this.getDetails);
+  // }
+
+  pushArr() {
+    this.arrs.push({
+      time: Date(),
+      state: this.show,
+    });
+    console.log(this.arrs);
+
+  }
 }
